0b7bae001d3c1b24b4ec62622b7e9b29a613a9f6
Marco Manieri (ufficio) <marco@italiatekno.it>
2019-07-03 15:36:02 +0200

    one @match to rule all them

--- crosslinks-enhancements.meta.js
+++ crosslinks-enhancements.meta.js
@@ -3,16 +3,13 @@
 // @name           Crosslinks Enhancements
 // @description    Adds options to limit cross links detection by drawn items color and to cross links inside a polygon
 // @category       Misc
-// @version        1.2
+// @version        1.3
 // @author         MarcioPG
 // @website        https://github.com/manierim/crosslinks-enhancements
 // @updateURL      https://github.com/manierim/crosslinks-enhancements/raw/master/crosslinks-enhancements.meta.js
 // @downloadURL    https://github.com/manierim/crosslinks-enhancements/raw/master/crosslinks-enhancements.user.js
 // @namespace      https://github.com/manierim
-// @include        *://*.ingress.com/intel*
-// @include        *://intel.ingress.com/*
-// @match          *://*.ingress.com/intel*
-// @match          *://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // @require        https://cdn.rawgit.com/hayeswise/Leaflet.PointInPolygon/v1.0.0/wise-leaflet-pip.js
 // ==/UserScript==
